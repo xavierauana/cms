f83c72a67cc501875f37992e47dd796c89d86ab2
Xavier Au <xavier.au@anacreation.com>
2018-08-04 22:17:20 +0800

    update pagae events

--- src/CmsEventServiceProvider.php
+++ src/CmsEventServiceProvider.php
@@ -8,6 +8,8 @@ use Anacreation\Cms\Events\LinkDeleted;
 use Anacreation\Cms\Events\LinkSaved;
 use Anacreation\Cms\Events\MenuDeleted;
 use Anacreation\Cms\Events\MenuSaved;
+use Anacreation\Cms\Events\PageDeleted;
+use Anacreation\Cms\Events\PageSaved;
 use Anacreation\Cms\Listeners\UpdateCacheHandler;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -35,6 +37,12 @@ class CmsEventServiceProvider extends ServiceProvider
             UpdateCacheHandler::class,
         ],
         LanguageDeleted::class => [
+            UpdateCacheHandler::class,
+        ],
+        PageSaved::class       => [
+            UpdateCacheHandler::class,
+        ],
+        PageDeleted::class     => [
             UpdateCacheHandler::class,
         ],
     ];
