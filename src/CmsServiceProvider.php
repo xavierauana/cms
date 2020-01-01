fcb96f53d128d85a528abc1e6e4e88a4d0913747
Xavier Au <xavier.au@anacreation.com>
2020-01-01 13:19:26 +0800

    update

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -10,7 +10,6 @@ use Anacreation\Cms\Contracts\ICreateContentObjectFromRequest;
 use Anacreation\Cms\Handler\Handler;
 use Anacreation\Cms\Services\CreateContentObjectFromRequest;
 use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -29,13 +28,12 @@ class CmsServiceProvider extends ServiceProvider
      */
     public function boot() {
 
-        Blade::doubleEncode();
 
-        if ($this->requestFrontend()) {
+        if($this->requestFrontend()) {
             $this->registerFrontendViewComposers();
         }
 
-        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
 
         $this->views();
 
@@ -48,6 +46,7 @@ class CmsServiceProvider extends ServiceProvider
         $this->registerConsoleCommands();
 
         $this->registerBladeDirectives();
+        
     }
 
     /**
@@ -55,58 +54,68 @@ class CmsServiceProvider extends ServiceProvider
      */
     public function register() {
         $this->mergeConfigFrom(
-            __DIR__ . '/config/cms.php', 'cms'
+            __DIR__.'/config/cms.php',
+            'cms'
         );
         $this->mergeConfigFrom(
-            __DIR__ . '/config/admin.php', 'admin'
+            __DIR__.'/config/admin.php',
+            'admin'
         );
 
         $this->registerBindings();
     }
 
     private function views() {
-        $this->loadViewsFrom(__DIR__ . '/views', 'cms');
+        $this->loadViewsFrom(__DIR__.'/views',
+                             'cms');
 
         $this->publishes([
-            __DIR__ . '/views' => resource_path('views/vendor/cms'),
-        ], 'backend_views');
+                             __DIR__.'/views' => resource_path('views/vendor/cms'),
+                         ],
+                         'backend_views');
     }
 
     private function config() {
         $this->publishes([
-            __DIR__ . '/config/cms.php'   => config_path('cms.php'),
-            __DIR__ . '/config/lfm.php'   => config_path('lfm.php'),
-            __DIR__ . '/config/admin.php' => config_path('admin.php'),
-        ], 'config');
+                             __DIR__.'/config/cms.php'   => config_path('cms.php'),
+                             __DIR__.'/config/lfm.php'   => config_path('lfm.php'),
+                             __DIR__.'/config/admin.php' => config_path('admin.php'),
+                         ],
+                         'config');
     }
 
     private function defaultAsset() {
         $this->publishes([
-            __DIR__ . '/public/css'      => public_path('css/cms'),
-            __DIR__ . '/public/js'       => public_path('js/cms'),
-            __DIR__ . '/public/ckeditor' => public_path('vendor/cms/ckeditor'),
-            __DIR__ . '/../fonts'        => public_path('fonts'),
-            __DIR__ . '/seeds'           => database_path('seeds'),
-            __DIR__ . '/themes'          => resource_path('views/themes'),
-        ], 'assets');
+                             __DIR__.'/public/css'      => public_path('css/cms'),
+                             __DIR__.'/public/js'       => public_path('js/cms'),
+                             __DIR__.'/public/ckeditor' => public_path('vendor/cms/ckeditor'),
+                             __DIR__.'/../fonts'        => public_path('fonts'),
+                             __DIR__.'/seeds'           => database_path('seeds'),
+                             __DIR__.'/themes'          => resource_path('views/themes'),
+                         ],
+                         'assets');
 
         $this->publishes([
-            __DIR__ . '/resources' => resource_path('cms'),
-        ], 'dev');
+                             __DIR__.'/resources' => resource_path('cms'),
+                         ],
+                         'dev');
     }
 
     private function registerBindings() {
 
-        app()->bind(ExceptionHandler::class, Handler::class);
+        app()->bind(ExceptionHandler::class,
+                    Handler::class);
 
-        foreach (config('cms.bindings') as $abstract => $implementation) {
-            app()->bind($abstract, $implementation);
+        foreach(config('cms.bindings') as $abstract => $implementation) {
+            app()->bind($abstract,
+                        $implementation);
         }
 
         app()->bind(ICreateContentObjectFromRequest::class,
-            CreateContentObjectFromRequest::class);
+                    CreateContentObjectFromRequest::class);
 
-        Route::model('page', get_class(app(Page::class)));
+        Route::model('page',
+                     get_class(app(Page::class)));
     }
 
     private function requestFrontend(): bool {
@@ -116,8 +125,10 @@ class CmsServiceProvider extends ServiceProvider
     }
 
     private function registerFrontendViewComposers() {
-        foreach (config('cms.view_composer', []) as $view => $composer) {
-            View::composer($view, $composer);
+        foreach(config('cms.view_composer',
+                       []) as $view => $composer) {
+            View::composer($view,
+                           $composer);
         }
     }
 
@@ -126,7 +137,7 @@ class CmsServiceProvider extends ServiceProvider
     }
 
     private function registerConsoleCommands() {
-        if ($this->app->runningInConsole()) {
+        if($this->app->runningInConsole()) {
             $this->commands($this->commands);
         }
     }
